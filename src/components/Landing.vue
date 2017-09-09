1fe66e20cf719b0e7b5fada1a7129c0b149028d2
Dummyc0m <m.steve.jobs@me.com>
2017-09-08 23:36:35 -0400

    added landing, home, and result routes

---
+++ src/components/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    created () {
+      this.$router.push({
+        name: 'home'
+      })
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Landing from '../components/Landing.vue'
+import Home from '../components/Home.vue'
+import Result from '../components/Result.vue'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'landing',
+      component: Landing
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/result',
+      name: 'result',
+      component: Result
     }
   ]
 })

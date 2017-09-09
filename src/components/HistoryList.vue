94a55621c3a4f684f869d0cbe6ad4fc8b6cf165a
Sam Yong <yongmc.sam2014@outlook.com>
2017-09-08 23:20:10 -0400

    history list demo

---
+++ src/components/HistoryList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <md-list>
+      <md-list-item>
+        <span>Fake History 1</span>
+      </md-list-item>
+
+      <md-list-item>
+        <span>Fake History 2</span>
+      </md-list-item>
+
+      <md-list-item>
+        <span>Fake History 3</span>
+      </md-list-item>
+
+      <md-list-item>
+        <span>Fake History 4</span>
+      </md-list-item>
+    </md-list>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import HistoryList from '../components/HistoryList.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/a',
+      name: 'a',
+      component: HistoryList
     }
   ]
 })

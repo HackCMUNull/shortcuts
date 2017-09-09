f23005f41233d7148b6468a61cbd196f5edf47af
hgu <hguhelen@gmail.com>
2017-09-09 00:12:28 -0400

    Merge branch 'master' of github.com:HackCMUNull/shortcuts
    # Conflicts:
    #	src/router/index.js

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,13 @@
   <div>
     <md-toolbar class="md-accent">
       <md-button class="md-icon-button">
-        <md-icon>menu</md-icon>
+        <md-icon>xxx</md-icon>
       </md-button>
 
       <h2 class="md-title" style="flex: 1">CMU Shortcut</h2>
 
       <md-button class="md-icon-button">
-        <md-icon>favorite</md-icon>
+        <md-icon>hey</md-icon>
       </md-button>
     </md-toolbar>
   </div>

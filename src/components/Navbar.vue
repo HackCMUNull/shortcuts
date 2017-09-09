6b8aa8704ea08e2b3b09badc994468ce1bfe4e07
hgu <hguhelen@gmail.com>
2017-09-09 00:11:28 -0400

    Merge branch 'master' of github.com:HackCMUNull/shortcuts
    # Conflicts:
    #	src/router/index.js

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <md-toolbar class="md-accent">
+      <md-button class="md-icon-button">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title" style="flex: 1">CMU Shortcut</h2>
+
+      <md-button class="md-icon-button">
+        <md-icon>favorite</md-icon>
+      </md-button>
+    </md-toolbar>
+  </div>
+</template>
+
+
+<script>
+  export default {}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-<<<<<<< HEAD
-import navbar from '../components/navbar.vue'
-=======
 import HistoryList from '../components/HistoryList.vue'
 import Landing from '../components/Landing.vue'
 import Home from '../components/Home.vue'
 import Result from '../components/Result.vue'
->>>>>>> 334c0e8673b1bfc96af59afba6ed7a506a0d5d68
+import Navbar from '../components/Navbar.vue'
 
 Vue.use(Router)
 
@@ -29,18 +25,15 @@ export default new Router({
       name: 'result',
       component: Result
     },
-<<<<<<< HEAD
-
     {
       path: '/b',
       name: 'b',
-      component: navbar
-=======
+      component: Navbar
+    },
     {
       path: '/a',
       name: 'a',
       component: HistoryList
->>>>>>> 334c0e8673b1bfc96af59afba6ed7a506a0d5d68
     }
   ]
 })
